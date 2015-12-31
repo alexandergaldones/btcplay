6f4b4a87ee4e3fdd5ef950093c277ddea6e60e43
xanderyui <alexander.galdones@gmail.com>
2016-01-01 03:53:07 +0800

    refresh time to 50s

--- btcplay/public/scripts/site_custom.js
+++ btcplay/public/scripts/site_custom.js
@@ -1,14 +1,14 @@
 $(function(){
 	setInterval(function(){
 		get_btcprice();
-	}, 20000);
+	}, 50000);
 	function get_btcprice(){
 	    var feedback = $.ajax({
 	        type: "GET",
 	        url: "/price",
 	        async: true
 	    }).success(function(data){
-	        setTimeout(function(){get_btcprice();}, 20000);
+	        setTimeout(function(){get_btcprice();}, 50000);
 	        $('.bitfinex').html(data.prices.bitfinex.last_price);
 	        $('.coinsph').html(data.prices.coinsph.price);
 	        $('.blockchaininfo').html(data.prices.blockchaininfo.price);
