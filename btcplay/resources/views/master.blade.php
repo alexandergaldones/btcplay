4d5520cccb30d43ca522061e99da7508aa553743
xanderyui <alexander.galdones@gmail.com>
2016-01-01 03:58:28 +0800

    removed topbar menu

--- btcplay/resources/views/master.blade.php
+++ btcplay/resources/views/master.blade.php
@@ -17,7 +17,7 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
       <!-- BEGIN TOP NAVIGATION BAR -->
       <div class="header-inner">
          <!-- BEGIN LOGO -->  
-         <a class="navbar-brand" href="index.html">
+         <a class="navbar-brand" href="/">
          <img src="img/logo.png" alt="logo" class="img-responsive" />
          </a>
          <!-- END LOGO -->
@@ -36,20 +36,12 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
                <i class="icon-angle-down"></i>
                </a>
                <ul class="dropdown-menu">
-                  <li><a href="extra_profile.html"><i class="icon-user"></i> My Profile</a>
-                  </li>
-                  <li><a href="page_calendar.html"><i class="icon-calendar"></i> My Calendar</a>
-                  </li>
-                  <li><a href="inbox.html"><i class="icon-envelope"></i> My Inbox <span class="badge badge-danger">3</span></a>
-                  </li>
-                  <li><a href="#"><i class="icon-tasks"></i> My Tasks <span class="badge badge-success">7</span></a>
-                  </li>
+                  <li><a href="#"><i class="icon-user"></i> My Profile</a>
+                  </li>                  
                   <li class="divider"></li>
                   <li><a href="javascript:;" id="trigger_fullscreen"><i class="icon-move"></i> Full Screen</a>
                   </li>
-                  <li><a href="extra_lock.html"><i class="icon-lock"></i> Lock Screen</a>
-                  </li>
-                  <li><a href="login.html"><i class="icon-key"></i> Log Out</a>
+                  <li><a href="#"><i class="icon-key"></i> Log Out</a>
                   </li>
                </ul>
             </li>
