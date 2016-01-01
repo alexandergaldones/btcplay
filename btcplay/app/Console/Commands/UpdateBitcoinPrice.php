31c596a70cf36398d1ceadaaa4b0d461b9233b8a
xanderyui <alexander.galdones@gmail.com>
2016-01-01 19:17:03 +0800

    new file crontab for price

---
+++ btcplay/app/Console/Commands/UpdateBitcoinPrice.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\PriceController;
+use Illuminate\Console\Command;
+
+class UpdateBitcoinPrice extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'btc:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function fire()
+    {
+        $this->info("Updating Bitcoin prices...");
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info("Updating Bitcoin prices...     :        " . date('Y-m-d H:i:s'));
+
+        $btcprice = new PriceController();
+        $btcprice->showPrices();
+        $this->comment('running..\n');    
+            
+        $this->comment('Finished at ' . date('Y-m-d H:i:s'));        
+    }
+}
