a493f1a0bb40344e3100e9f16ef3f5fec89cb565
xanderyui <alexander.galdones@gmail.com>
2016-01-02 22:24:12 +0800

    added console for headlines caching

---
+++ btcplay/app/Console/Commands/Headliners.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Http\Controllers\PriceController;
+
+class Headliners extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'btc:headline';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->comment('Caching Headlines       :       ' . date('Y-m-d H:i:s'));
+        $controller = new PriceController();
+        $controller->getHeadlines();  
+        $this->comment('done. waiting for the next run...');
+    }
+}
